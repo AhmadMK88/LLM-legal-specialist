f1726033e3710739704fdf733d5a8224402a80cc
Ahmad Mokayes <ahmadmokayes101@gmail.com>
2025-11-29 14:51:05 +0300

    Added model configs in a seperate file

---
+++ config/configs.py
@@ -0,0 +1,23 @@
+from peft import LoraConfig
+from transformers import TrainingArguments
+
+LORA_CONFIG = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    lora_dropout=0.05,
+    target_modules=["q_proj", "v_proj", "k_proj", "o_proj"],
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+TRAINING_ARGS = TrainingArguments(
+    output_dir="legal-qwen25-7b",
+    per_device_train_batch_size=1,
+    gradient_accumulation_steps=8,
+    num_train_epochs=2,
+    learning_rate=2e-4,
+    logging_steps=5,
+    save_steps=200,
+    fp16=True,
+    report_to="none",
+)
