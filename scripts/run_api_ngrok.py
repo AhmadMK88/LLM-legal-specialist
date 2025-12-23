cce0bf39247d22e1e34274e4bc0c517de7d5c758
Ahmad Mokayes <ahmadmokayes101@gmail.com>
2025-12-23 15:05:05 +0300

    test

--- scripts/run_api_ngrok.py
+++ scripts/run_api_ngrok.py
@@ -1,30 +1,22 @@
-import socket, threading, time
+import nest_asyncio
+nest_asyncio.apply()
+
+import socket, time
 import uvicorn
 from pyngrok import ngrok, conf
 from api.server import app
 from config.configs import NGROK_TOKEN
 
-
 def free_port():
     s = socket.socket()
-    s.bind(('', 0))
+    s.bind(("", 0))
     port = s.getsockname()[1]
     s.close()
     return port
 
-
 port = free_port()
-
-conf.get_default().ngrok_path = "./ngrok"
 conf.get_default().auth_token = NGROK_TOKEN
+public_url = ngrok.connect(port)
+print("🚀 Public API URL:", public_url.public_url + "/generate")
 
-public_url = ngrok.connect(port).public_url
-print("🚀 Public API URL:", public_url + "/generate")
-
-
-def run():
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
-
-threading.Thread(target=run, daemon=True).start()
-time.sleep(1)
+uvicorn.run(app, host="0.0.0.0", port=port)
